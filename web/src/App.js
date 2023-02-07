6bd2348c20294e7cf24cc81cf4032a34d6addbf1
randry claude <randryc7@gmail.com>
2023-02-07 18:03:17 +0100

    mon super dasboard
    enfin on a reussi a mettre en place la communication arduino-react, il nous reste a travailler le style et autres fonctionnalite

--- web/src/App.js
+++ web/src/App.js
@@ -4,38 +4,15 @@ import Top_nav_bar from "./components/Top_nav_bar"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Arduino from "./components/Arduino";
-import Client from "./components/client"
 import React from "react";
-
 
 class App extends React.Component{
 
   constructor(props){
     super(props);
-
-    this.state = {
-      data:""
-    }
-  }
+   }
 
   render(){
-
-    Client.client.on('connect', () => {
-      console.log('Client connected:')
-      // Subscribe
-      Client.client.subscribe('gyro_x', { qos: 0 });
-    })
-    
-    // Received
-    Client.client.on('message', (topic, message, packet) => {
-      // message is Buffer
-      // console.log(message.toString());
-      this.setState({
-        data:message.toString()
-      });
-      console.log('Received Message: ' + message.toString() + '\nOn topic: ' + topic)
-    })
-
 
     return(
       <>
@@ -48,7 +25,7 @@ class App extends React.Component{
         <Route path="/home" element={<Sous_container composant={<Home/>} />}></Route>
         <Route path="/workspace" element={
           <div id="sous_container1">
-            {<Arduino data={this.state.data}  />}
+            {<Arduino/>}
           </div>
         }></Route>
       </Routes>
